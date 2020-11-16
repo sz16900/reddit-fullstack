4d04780b0fc84069c56f701ccefbafb632039ce4
sz16900 <sz16900@my.bristol.ac.uk>
2020-11-16 10:29:37 -1000

    finish setup apollo graphql

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -3,17 +3,23 @@ import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import path from 'path'
 
+// require('dotenv').config()
+// const user = process.env.USER;
+// const password = process.env.PASSWORD;
+
+// console.log('PASS = ', typeof password)
+
 export default {
     migrations: {
         path: path.join(__dirname, './migrations'), // path to the folder with migrations
         pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
         
     },
-    entities: [Post],
+    entities: [Post], 
     dbName: 'reddit-fullstack',
     type: 'postgresql',
     // this was might quick fix, not very elegant
     user: 'postgres',
-    password:'postgres',
+    password: 'postgres',
     debug: !__prod__,
 } as Parameters<typeof MikroORM.init>[0];
