db5b1570da005074f064106561520bdc07798ff0
sz16900 <sz16900@my.bristol.ac.uk>
2020-11-16 09:16:49 -1000

    add user and password

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,19 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import path from 'path'
+
+export default {
+    migrations: {
+        path: path.join(__dirname, './migrations'), // path to the folder with migrations
+        pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
+        
+    },
+    entities: [Post],
+    dbName: 'reddit-fullstack',
+    type: 'postgresql',
+    // this was might quick fix, not very elegant
+    user: 'postgres',
+    password:'postgres',
+    debug: !__prod__,
+} as Parameters<typeof MikroORM.init>[0];
